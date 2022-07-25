538cf0b187df091a340d9c7688cc270aca67c68a
AndreGuilhermePC <andreguilhermepc@gmail.com>
2022-07-25 18:24:16 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/Sellerdao.java
+++ src/model/dao/Sellerdao.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 import model.entities.Seller;
 
-public interface Sellerdao {
+public interface SellerDao {
 	//Operação responsável por inserir esse obj no banco de dados que vai ser enviado como parâmentro de entrada 
 	void insert(Seller obj);
 	
 	void update(Seller obj);
 	void deleteById(Integer id);
-
+	
 	//Essa operação vai ser responsável por pegar ID e consultar no banco de dados um obj que tenha esse ID, se existir vai retornar e se não retorna null 
 	Seller findById(Integer id);
 	
 	//Para retornar todos os departamentos
-	List<Seller> findAll();
+	List<Seller> finadAll();
 }
